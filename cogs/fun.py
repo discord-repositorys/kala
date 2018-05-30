a92620523d37bde12a54d6c2bacb578878993cc8
PIKAAAAAAAAACHUUUUUUUUUUU <36680137+BloodyPikachu@users.noreply.github.com>
2018-05-30 13:10:17 -0500

    ack rip succ acc

--- cogs/fun.py
+++ cogs/fun.py
@@ -32,7 +32,7 @@ class Fun:
 		
       num = random.randint(1, 926)
       async with aiohttp.ClientSession().get(f'https://pokeapi.co/api/v2/pokemon-form/{num}/') as resp:
-    	data = await resp.json()
+        data = await resp.json()
       embed = discord.Embed(title="Who\'s that pokemon!?", color = ctx.author.color)
       embed.set_image(url=data['sprites']['front_default'])
       await ctx.send(embed = embed)
