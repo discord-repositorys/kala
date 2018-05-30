a622f4ce9a1a22d1022de7101c8d849ea7d4e510
PIKAAAAAAAAACHUUUUUUUUUUU <36680137+BloodyPikachu@users.noreply.github.com>
2018-05-29 19:00:27 -0500

    added toprole & check perms for user

--- cogs/utility.py
+++ cogs/utility.py
@@ -361,6 +361,39 @@ class Utility:
         embed.add_field(name="Example", value=result['example'], inline=True)
         embed.set_footer(text=u"👍 " + str(result['thumbs_up']) + " | " + u"👎 " + str(result['thumbs_down']))
         await ctx.send(embed=embed)
+        
+    @commands.command(name='top_role', aliases=['toprole'])
+    @commands.guild_only()
+    async def show_toprole(self, ctx, *, member: discord.Member=None):
+        """Simple command which shows the members Top Role."""
+
+        if member is None:
+            member = ctx.author
+
+        embed = discord.Embed(title="Highest role")
+        embed.description = f'The top role for {member.display_name} is {member.top_role.name}'
+        await ctx.send(embed=embed)
+    
+    @commands.command(name='perms', aliases=['perms_for', 'permissions'])
+    @commands.guild_only()
+    async def check_permissions(self, ctx, *, member: discord.Member=None):
+        """A simple command which checks a members Guild Permissions.
+        If member is not provided, the author will be checked."""
+
+        if not member:
+            member = ctx.author
+
+        
+        perms = '\n'.join(perm for perm, value in member.guild_permissions if value)
+
+        
+        embed = discord.Embed(title='Permissions for:', description=ctx.guild.name, colour=member.colour)
+        embed.set_author(icon_url=member.avatar_url, name=str(member))
+
+        
+        embed.add_field(name='\uFEFF', value=perms)
+
+        await ctx.send(content=None, embed=embed)
     
 
 def setup(bot):
