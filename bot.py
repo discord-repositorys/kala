898cb060cd290edbabd3d894c524ec76b791b444
PIKAAAAAAAAACHUUUUUUUUUUU <36680137+BloodyPikachu@users.noreply.github.com>
2018-05-30 02:36:20 -0500

    added eval and cleancode

--- bot.py
+++ bot.py
@@ -39,7 +39,24 @@ async def save_prefix(prefix, guildID, ctx):
 bot = commands.Bot(command_prefix=getprefix, owner_id=426060491681431562)
 bot.commands_run = 0
 
+def cleanup_code(content):
+    '''Automatically removes code blocks from the code.'''
+    # remove ```py\n```
+    if content.startswith('```') and content.endswith('```'):
+        return '\n'.join(content.split('\n')[1:-1])
 
+    return content.strip('` \n')
+
+if 'TOKEN' in os.environ:
+    heroku = True
+    TOKEN = os.environ['TOKEN']
+
+def dev_check(id):
+    with open('data/devs.json') as f:
+        devs = json.load(f)
+        if id in devs:
+            return True
+        return False
 
 @bot.event
 async def on_ready():
@@ -93,6 +110,90 @@ bot.load_extension("cogs.fun")
 bot.load_extension("cogs.nsfw")
 bot.load_extension("cogs.meta")
 
+@bot.command(name='eval')
+async def _eval(ctx, *, body):
+    """Evaluates python code"""
+    if not dev_check(ctx.author.id):
+        return await ctx.send("You cannot use this because you are not a developer.")
+    env = {
+        'ctx': ctx,
+        'channel': ctx.channel,
+        'author': ctx.author,
+        'guild': ctx.guild,
+        'message': ctx.message,
+        '_': bot._last_result,
+        'source': inspect.getsource
+    }
+
+    env.update(globals())
+
+    body = cleanup_code(body)
+    stdout = io.StringIO()
+    err = out = None
+
+    to_compile = f'async def func():\n{textwrap.indent(body, "  ")}'
+
+    def paginate(text: str):
+        '''Simple generator that paginates text.'''
+        last = 0
+        pages = []
+        for curr in range(0, len(text)):
+            if curr % 1980 == 0:
+                pages.append(text[last:curr])
+                last = curr
+                appd_index = curr
+        if appd_index != len(text) - 1:
+            pages.append(text[last:curr])
+        return list(filter(lambda a: a != '', pages))
+
+    try:
+        exec(to_compile, env)
+    except Exception as e:
+        err = await ctx.send(f'```py\n{e.__class__.__name__}: {e}\n```')
+        return await ctx.message.add_reaction('\u2049')
+
+    func = env['func']
+    try:
+        with redirect_stdout(stdout):
+            ret = await func()
+    except Exception as e:
+        value = stdout.getvalue()
+        err = await ctx.send(f'```py\n{value}{traceback.format_exc()}\n```')
+    else:
+        value = stdout.getvalue()
+        if TOKEN in value:
+            value = value.replace(TOKEN,"[lol]")
+        if ret is None:
+            if value:
+                try:
+
+                    out = await ctx.send(f'```py\n{value}\n```')
+                except:
+                    paginated_text = paginate(value)
+                    for page in paginated_text:
+                        if page == paginated_text[-1]:
+                            out = await ctx.send(f'```py\n{page}\n```')
+                            break
+                        await ctx.send(f'```py\n{page}\n```')
+        else:
+            bot._last_result = ret
+            try:
+                out = await ctx.send(f'```py\n{value}{ret}\n```')
+            except:
+                paginated_text = paginate(f"{value}{ret}")
+                for page in paginated_text:
+                    if page == paginated_text[-1]:
+                        out = await ctx.send(f'```py\n{page}\n```')
+                        break
+                    await ctx.send(f'```py\n{page}\n```')
+
+    if out:
+        await ctx.message.add_reaction('\u2705')  # tick
+    elif err:
+        await ctx.message.add_reaction('\u2049')  # x
+    else:
+        await ctx.message.add_reaction('\u2705')
+
 @bot.command()
 @commands.has_permissions(manage_messages=True)
 async def prefix(ctx, prefix=None):
@@ -134,7 +235,7 @@ async def ping(ctx):
     msg = await ctx.send(embed=e)
     em = discord.Embed(color=color, title='Ping! Bot Latency is: ')
     em.description = f"{bot.latency * 1000:.4f} ms"
-    em.set_thumbnail(url="https://giphy.com/gifs/tennis-4IAzyrhy9rkis ")
+    em.set_thumbnail(url="https://giphy.com/gifs/cat-animal-ping-pong-fvA1ieS8rEV8Y")
     await msg.edit(embed=em)
     
 @bot.command()
