41847e80b2dd25f85e4ba46ca568dd1e2abcb0bf
PIKAAAAAAAAACHUUUUUUUUUUU <36680137+BloodyPikachu@users.noreply.github.com>
2018-05-30 01:08:49 -0500

    oh man

--- cogs/info.py
+++ cogs/info.py
@@ -100,7 +100,7 @@ class Info:
         server_embed.add_field(name='Server ID', value=str(guild.id))
         server_embed.add_field(name='Owner', value=guild.owner.name + "#" + guild.owner.discriminator)
         server_embed.add_field(name='Members ({:,} total)'.format(len(guild.members), value=user_string)
-        server_embed.add_field(name=f'Channel Categories', value=len(guild.categories))
+        server_embed.add_field(name='Channel Categories', value=len(guild.categories))
         chandesc = "{;,} text, {;,} voice".format(len(guild..text_channels), len(guild.voice_channels)
         server_embed.add_field(name='Total Channels', value=chandesc)
         server_embed.add_field(name="AFK Channel & Time:", value = f"Channel: **{ctx.guild.afk_channel}**" "Time: **{} minutes**".format(int(ctx.guild.afk_timeout / 60)))
