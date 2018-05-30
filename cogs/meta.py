8a4fb3b3c77b922f05ab005bb312b25b4407a43b
PIKAAAAAAAAACHUUUUUUUUUUU <36680137+BloodyPikachu@users.noreply.github.com>
2018-05-29 23:54:40 -0500

    skra

--- cogs/meta.py
+++ cogs/meta.py
@@ -50,5 +50,36 @@ class Meta:
         perms.add_reactions = True
         await ctx.send(f'<{discord.utils.oauth_url(self.bot.client_id, perms)}>')
         
+    @commands.command(aliases=['google'])
+    async def g(self, ctx, *, query):
+        """Searches google and gives you top result."""
+        await ctx.trigger_typing()
+        try:
+            card, entries = await self.get_google_entries(query)
+        except RuntimeError as e:
+            await ctx.send(str(e))
+        else:
+            if card:
+                value = '\n'.join(f'[{title}]({url.replace(")", "%29")})' for url, title in entries[:3])
+                if value:
+                    card.add_field(name='Search Results', value=value, inline=False)
+                return await ctx.send(embed=card)
+
+            if len(entries) == 0:
+                return await ctx.send('No results found... sorry.')
+
+            next_two = [x[0] for x in entries[1:3]]
+            first_entry = entries[0][0]
+            if first_entry[-1] == ')':
+                first_entry = first_entry[:-1] + '%29'
+
+            if next_two:
+                formatted = '\n'.join(f'<{x}>' for x in next_two)
+                msg = f'{first_entry}\n\n**See also:**\n{formatted}'
+            else:
+                msg = first_entry
+
+            await ctx.send(msg)
+        
 def setup(bot):
     bot.add_cog(Meta(bot))
