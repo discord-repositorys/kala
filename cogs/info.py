9fd314ead49c7f0cfe1c0c9ddeb853dfe3c69007
PIKAAAAAAAAACHUUUUUUUUUUU <36680137+BloodyPikachu@users.noreply.github.com>
2018-05-30 01:29:20 -0500

    Update info.py

--- cogs/info.py
+++ cogs/info.py
@@ -99,7 +99,7 @@ class Info:
         server_embed.add_field(name='Server ID', value=str(guild.id))
         server_embed.add_field(name='Owner', value=guild.owner.name + "#" + guild.owner.discriminator)
         server_embed.add_field(name='Members ({:,} total)'.format(len(guild.members), value=user_string)
-        server_embed.add_field(name="Channels", value=textchannels + voicechannels)
+        server_embed.add_field(name='Channels', value=textchannels + voicechannels)
         server_embed.add_field(name="AFK Channel & Time:", value = f"Channel: **{ctx.guild.afk_channel}**" "Time: **{} minutes**".format(int(ctx.guild.afk_timeout / 60)))
         server_embed.add_field(name='Emoji Count', value=regular_emojis + animated_emojis)
         server_embed.add_field(name='Normal Emojis', value=regular_emojis)
