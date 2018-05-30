c86efc16b185a314651a7c687f48f8ba2e303637
PIKAAAAAAAAACHUUUUUUUUUUU <36680137+BloodyPikachu@users.noreply.github.com>
2018-05-30 11:27:13 -0500

    lel

--- cogs/fun.py
+++ cogs/fun.py
@@ -20,13 +20,13 @@ class Fun:
         self.bot = bot
 	
     @commands.command()
-    async def cat(self, ctx):
-        async with aiohttp.ClientSession() as session:
-	    async with session.get('http://random.cat/meow') as resp:
-	        data = await resp.json()
-	    embed = discord.Embed(color=discord.Color.blue())
-	    embed.set_image(url=data['file'])
-            await ctx.send(embed=embed)
+	async def cat(self, ctx):
+		async with aiohttp.ClientSession() as session:
+			async with session.get('http://random.cat/meow') as resp:
+				data = await resp.json()
+		embed = discord.Embed(color=discord.Color.blue())
+		embed.set_image(url=data['file'])
+		await ctx.send(embed=embed)
 
 
     @commands.command()
