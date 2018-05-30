8a7c059f9fd3f0d5960f739bc08dd379eb9d9bd4
PIKAAAAAAAAACHUUUUUUUUUUU <36680137+BloodyPikachu@users.noreply.github.com>
2018-05-29 22:51:23 -0500

    ye

---
+++ cogs/meta.py
@@ -0,0 +1,33 @@
+import discord
+from discord.ext import commands
+import random
+import asyncio
+import datetime
+import time
+import traceback
+import sys
+import aiohttp
+import urllib.parse
+
+class Meta:
+    """Meta Commands"""
+    def __init__(self, bot):
+        self.bot = bot
+        
+    @commands.command()
+    async def charinfo(self, ctx, *, characters: str):
+        """Shows you information about a number of characters.
+        Only up to 25 characters at a time.
+        """
+
+        def to_string(c):
+            digit = f'{ord(c):x}'
+            name = unicodedata.name(c, 'Name not found.')
+            return f'`\\U{digit:>08}`: {name} - {c} \N{EM DASH} <http://www.fileformat.info/info/unicode/char/{digit}>'
+        msg = '\n'.join(map(to_string, characters))
+        if len(msg) > 2000:
+            return await ctx.send('Output too long to display.')
+        await ctx.send(msg)
+        
+def setup(bot):
+    bot.add_cog(Meta(bot)
