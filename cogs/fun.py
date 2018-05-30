8d257e38875780bf45f78d57b542d8ab428325f1
PIKAAAAAAAAACHUUUUUUUUUUU <36680137+BloodyPikachu@users.noreply.github.com>
2018-05-30 02:18:25 -0500

    mayeb ascci

--- cogs/fun.py
+++ cogs/fun.py
@@ -126,32 +126,32 @@ class Fun:
         await ctx.send(embed=embed)
 
     @commands.command(pass_context=True, no_pm=True)
-	async def ascii(self, ctx, *, text : str = None):
-		"""Beautify some text (font list at http://artii.herokuapp.com/fonts_list)."""
+    async def ascii(self, ctx, *, text : str = None):
+        """Beautify some text (font list at http://artii.herokuapp.com/fonts_list)."""
 
-		if text == None:
-			await ctx.channel.send('Usage: `{}ascii [font (optional)] [text]`\n(font list at http://artii.herokuapp.com/fonts_list)'.format(ctx.prefix))
-			return
+	if text == None:
+		await ctx.channel.send('Usage: `{}ascii [font (optional)] [text]`\n(font list at http://artii.herokuapp.com/fonts_list)'.format(ctx.prefix))
+		return
 
-		# Get list of fonts
-		fonturl = "http://artii.herokuapp.com/fonts_list"
-		response = await DL.async_text(fonturl)
-		fonts = response.split()
+	# Get list of fonts
+	fonturl = "http://artii.herokuapp.com/fonts_list"
+	response = await DL.async_text(fonturl)
+	fonts = response.split()
 
-		font = None
-		# Split text by space - and see if the first word is a font
-		parts = text.split()
-		if len(parts) > 1:
-			# We have enough entries for a font
-			if parts[0] in fonts:
-				# We got a font!
-				font = parts[0]
-				text = ' '.join(parts[1:])
+	font = None
+	# Split text by space - and see if the first word is a font
+	parts = text.split()
+	if len(parts) > 1:
+		# We have enough entries for a font
+		if parts[0] in fonts:
+			# We got a font!
+			font = parts[0]
+			text = ' '.join(parts[1:])
 	
-		url = "http://artii.herokuapp.com/make?{}".format(urllib.parse.urlencode({'text':text}))
-		if font:
-			url += '&font={}'.format(font)
-		response = await DL.async_text(url)
+	url = "http://artii.herokuapp.com/make?{}".format(urllib.parse.urlencode({'text':text}))
+	if font:
+		url += '&font={}'.format(font)
+	response = await DL.async_text(url)
         await ctx.channel.send("```Markup\n{}```".format(response))
     
 
