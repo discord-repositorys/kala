133b0dbf5ab2744f5c4eb6ff519473332d07e91a
PIKAAAAAAAAACHUUUUUUUUUUU <36680137+BloodyPikachu@users.noreply.github.com>
2018-06-01 16:22:30 -0500

    largest servers

--- cogs/info.py
+++ cogs/info.py
@@ -15,7 +15,17 @@ class Info:
     def __init__(self, bot):
         self.bot = bot
     
-    
+    @commands.command(aliases=['ls'])
+    async def largestservers(self, ctx):
+        """Show the 5 largest servers the bot sees."""
+        servers = sorted(ctx.bot.guilds, key=lambda x: -len(x.members))
+
+        msg = ""
+        for i in range(0, 10):
+            msg += "{0}: {1} members.\n".format(
+                servers[i].name, len(servers[i].members))
+
+	await ctx.send(msg)
 
     @commands.command(pass_context=True, aliases=['si', 'sinfo'])
     async def serverinfo(self, ctx, *, guild_name = None):
