ae59d5017f9e8ec78e2ebf9b40493ed5f284970d
PIKAAAAAAAAACHUUUUUUUUUUU <36680137+BloodyPikachu@users.noreply.github.com>
2018-06-03 16:05:36 -0500

    skra

--- cogs/cr.py
+++ cogs/cr.py
@@ -11,7 +11,7 @@ from discord.ext import commands
 class CR:
     def __init__(self, bot):
         self.bot = bot
-        self.token = os.environ.get('CRAPI')
+        self.token = os.environ.get(['CRAPI'])
         self.client = clashroyale.Client(token=self.token, is_async=True)
 
 
