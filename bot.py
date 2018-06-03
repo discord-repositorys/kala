9c1f309c5bbbe68883db58e6fe128208ab03e258
PIKAAAAAAAAACHUUUUUUUUUUU <36680137+BloodyPikachu@users.noreply.github.com>
2018-06-03 16:09:03 -0500

    lol

--- bot.py
+++ bot.py
@@ -18,6 +18,7 @@ import aiohttp
 
 bravo_db = AsyncIOMotorClient(os.environ['DB'])
 cr_db = AsyncIOMotorClient(os.environ['cr_db'])
+bot.cr_db = db.clashroyaledb
 
 async def getprefix(bot, message):
     if isinstance(message.channel, discord.DMChannel): return "k."
