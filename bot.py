28cfea10af3e22a402901fc2dc124ca0144da1a0
PIKAAAAAAAAACHUUUUUUUUUUU <36680137+BloodyPikachu@users.noreply.github.com>
2018-06-03 15:39:51 -0500

    k

--- bot.py
+++ bot.py
@@ -72,7 +72,7 @@ async def on_ready():
         await asyncio.sleep(2)
         await bot.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name="you sleep"))
         await asyncio.sleep(2)
-        await bot.change_presence(activity=discord.Streaming(name="you sleep", url="https://www.twicth.tv/scarecrow"))
+        await bot.change_presence(activity=discord.Streaming(name="you sleep", url="https://www.twitch.tv/scarecrow"))
         await asyncio.sleep(2)
         
         
