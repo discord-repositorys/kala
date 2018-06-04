775e784adaacd1b619315e856f983658d5555c2b
PIKAAAAAAAAACHUUUUUUUUUUU <36680137+BloodyPikachu@users.noreply.github.com>
2018-06-03 19:19:25 -0500

    hm

--- bot.py
+++ bot.py
@@ -43,7 +43,7 @@ bot = commands.Bot(command_prefix=getprefix, owner_id=426060491681431562)
 bot._last_result = None
 bot.commands_run = 0
 bot.session = aiohttp.ClientSession()
-bot.cr_db = cr_db.datorangebot
+
 
 def cleanup_code(content):
     '''Automatically removes code blocks from the code.'''
