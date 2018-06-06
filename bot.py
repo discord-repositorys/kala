0de9c53a85bfed1d8387803a72fcbc206d95fab1
PIKAAAAAAAAACHUUUUUUUUUUU <36680137+BloodyPikachu@users.noreply.github.com>
2018-06-06 09:34:14 -0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -17,6 +17,7 @@ import aiohttp
 
 
 bravo_db = AsyncIOMotorClient(os.environ['DB'])
+cr_db = AsyncioMototClient(os.environ['cr_db'])
 
 
 
@@ -43,7 +44,7 @@ bot = commands.Bot(command_prefix=getprefix, owner_id=426060491681431562)
 bot._last_result = None
 bot.commands_run = 0
 bot.session = aiohttp.ClientSession()
-bot.db = AsyncIOMotorClient(os.environ['DB'])
+
 
 
 
