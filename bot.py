a78dc2b45dadbc614f37867dbfefa77a23f7adf4
PIKAAAAAAAAACHUUUUUUUUUUU <36680137+BloodyPikachu@users.noreply.github.com>
2018-06-07 19:26:00 -0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -124,6 +124,8 @@ bot.load_extension("cogs.owner")
 bot.load_extension("cogs.fun")
 bot.load_extension("cogs.latency")
 bot.load_extension("cogs.cr")
+bot.load_extension("cogs.messages")
+
 
 
 @bot.command(name='eval')
