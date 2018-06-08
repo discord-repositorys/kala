e5231d45070e8393e1358ab46e6aeda1689251b4
PIKAAAAAAAAACHUUUUUUUUUUU <36680137+BloodyPikachu@users.noreply.github.com>
2018-06-07 19:32:49 -0500

    lol

--- cogs/messages.py
+++ cogs/messages.py
@@ -3,7 +3,7 @@ from discord.ext import commands
 import datetime
 
 
-class Config:
+class Messages:
     '''Make some Welcome And Goodbye messages for your server!'''
 
     def __init__(self, bot):
